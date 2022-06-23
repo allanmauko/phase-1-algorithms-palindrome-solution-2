98c808127053ab76518a551e8b18586defc8812e
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-23 09:12:53 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+function reversedString(word){
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reversedString(word);
+  for(let startIndex = 0; startIndex <word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true
 }
 
 /* 
@@ -17,6 +29,26 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+console.log("");
+
+console.log("Expecting:true");
+console.log("=>", isPalindrome("abba"));
+
+console.log("");
+
+console.log("Expecting; true");
+console.log("=>", isPalindrome("a"));
+
+console.log("");
+
+console.log("Expecting:false");
+console.log("=>", isPalindrome("hi"))
+
+console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
